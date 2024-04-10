665b524e9bdd019a6749a271f23f3ac49e1e101b
Parimal <99127122+ArchProtios@users.noreply.github.com>
2024-04-10 23:15:30 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,9 @@
+const amount = 12
+
+if(amount < 20) {
+    console.log("Less");
+} else if(amount = 20) {
+    console.log("Equal");
+} else {
+    console.log("More");
+}
---
+++ 2.js
@@ -0,0 +1,10 @@
+// GLOBALS - NO WINDOWS
+
+// __dirname   - path to current directory
+// __filename  - path to file name
+// require     - is a function to import modules
+// module      - is a peice of reusable code (different types of modules in Node.js:- built-in modules(http,fs,os) ; modules you create yourself(by the use of require) ; modules created by other developers and shared via npm(example-axios))
+// process     - it is a global object that is used to provide information and control over the current node.js process
+
+console.log(__dirname);
+console.log(__filename);
